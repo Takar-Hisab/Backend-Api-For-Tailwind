e3e65668b792b3bfb9f80e8f3e7698c470236365
tushar-me <tusharimran.dev@gmial.com>
2024-05-12 14:27:59 +0600

    Product Migration Updated

--- database/migrations/2024_05_12_081816_create_products_table.php
+++ database/migrations/2024_05_12_081816_create_products_table.php
@@ -13,9 +13,22 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+
+            $table->foreignId('category_id')
+            ->constrained()
+            ->cascadeOnDelete()
+            ->cascadeOnUpdate();
+
+            $table->foreignId('brand_id')
+            ->constrained()
+            ->cascadeOnDelete()
+            ->cascadeOnUpdate();
+
             $table->string('sku')->unique()->index();
             $table->string('price');
             $table->string('quantity')->nullable();
+            $table->string('slug')->unique();
             $table->string('title')->nullable()->index();
             $table->longText('description')->nullable();
             $table->string('image')->nullable();
